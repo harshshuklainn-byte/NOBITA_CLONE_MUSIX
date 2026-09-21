15d59cb4bfba047fb007d5246ebed6e1b20d864e
harshshuklainn-byte <harsh.shukla.inn@gmail.com>
2026-09-21 08:38:17 +0530

    Add files via upload

--- Clonify/cplugin/clone.py
+++ Clonify/cplugin/clone.py
@@ -12,7 +12,7 @@ from .utils import StartTime
 from Clonify.utils import get_readable_time
 from Clonify.utils.decorators.language import language
 
-APP_LINK = f"https://t.me/@NobitaMusiccRobot"
+APP_LINK = f"https://t.me/SpotifyMusicLiveBot"
 
 
 @Client.on_message(filters.command("clone"))
