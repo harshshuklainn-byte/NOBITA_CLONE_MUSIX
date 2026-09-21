fc8daa58f800bcdccdeda28ad6709c177fd17697
harshshuklainn-byte <harsh.shukla.inn@gmail.com>
2026-09-21 10:25:11 +0530

    Add files via upload

--- Clonify/core/bot.py
+++ Clonify/core/bot.py
@@ -4,9 +4,6 @@ from pyrogram.enums import ChatMemberStatus
 import config
 
 from ..logging import LOGGER
-from Clonify.utils.premium_ui import decorate_caption, decorate_text
-
-
 class PRO(Client):
     def __init__(self):
         LOGGER(__name__).info("Starting Bot...")
@@ -26,6 +23,7 @@ class PRO(Client):
     # premium custom-emoji treatment. The guard in decorate_text()
     # prevents duplicate decoration during message edits.
     async def send_message(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_text
         if len(args) >= 2 and isinstance(args[1], str):
             args = (args[0], decorate_text(args[1], 3), *args[2:])
         elif isinstance(kwargs.get("text"), str):
@@ -33,6 +31,7 @@ class PRO(Client):
         return await super().send_message(*args, **kwargs)
 
     async def edit_message_text(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_text
         if len(args) >= 3 and isinstance(args[2], str):
             args = (args[0], args[1], decorate_text(args[2], 3), *args[3:])
         elif isinstance(kwargs.get("text"), str):
@@ -40,6 +39,7 @@ class PRO(Client):
         return await super().edit_message_text(*args, **kwargs)
 
     async def send_photo(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         elif len(args) >= 3 and isinstance(args[2], str):
@@ -47,6 +47,7 @@ class PRO(Client):
         return await super().send_photo(*args, **kwargs)
 
     async def edit_message_caption(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         elif len(args) >= 3 and isinstance(args[2], str):
@@ -54,26 +55,31 @@ class PRO(Client):
         return await super().edit_message_caption(*args, **kwargs)
 
     async def send_video(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         return await super().send_video(*args, **kwargs)
 
     async def send_audio(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         return await super().send_audio(*args, **kwargs)
 
     async def send_document(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         return await super().send_document(*args, **kwargs)
 
     async def send_animation(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         return await super().send_animation(*args, **kwargs)
 
     async def send_voice(self, *args, **kwargs):
+        from Clonify.utils.premium_ui import decorate_caption
         if isinstance(kwargs.get("caption"), str):
             kwargs["caption"] = decorate_caption(kwargs["caption"])
         return await super().send_voice(*args, **kwargs)
